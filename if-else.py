a8c689424897115336cff68768c1e5062cba9857
Johnny <syedtauhasshah@gmail.com>
2026-01-10 23:18:17 +0530

    DAY 1

---
+++ if-else.py
@@ -0,0 +1,10 @@
+#Condition statement : if(one condition) -else (2 conditions)
+#if(condition) -else (conditions).....elif(more than two conditions
+
+
+marks=int(input("Enter Marks"))
+
+if(marks>=40):
+    print("Pass")
+else:
+    print("Fail")
